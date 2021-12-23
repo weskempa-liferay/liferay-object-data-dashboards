7ea1d8fbdad320989a32ce9c83bb044cde183d55
lgdd <louis-guillaume.durand@liferay.com>
2021-12-23 16:09:05 +0100

    chore: format

--- src/common/services/liferay/api.js
+++ src/common/services/liferay/api.js
@@ -1,26 +1,27 @@
 const getAuthzHeaders = () => {
-	if(typeof(window['Liferay']) == 'undefined') {
-		const user = process.env.REACT_APP_LIFERAY_USER
+	if (typeof window['Liferay'] == 'undefined') {
+		const user = process.env.REACT_APP_LIFERAY_USER;
 		const password = process.env.REACT_APP_LIFERAY_PASSWORD;
-		const base64credentials = Buffer(`${user}:${password}`).toString('base64');
+		const base64credentials = Buffer(`${user}:${password}`).toString(
+			'base64'
+		);
 
 		// Basic Auth for local development testing only
 		// Auth token is the best method for live use and
 		// is available when acting as a Web Component (Custom Element)
 		return new Headers({
-	    	'Authorization': 'Basic ' + base64credentials, 
-			'Content-Type': 'application/json'
-	    });
+			'Authorization': 'Basic ' + base64credentials,
+			'Content-Type': 'application/json',
+		});
 	}
 	return new Headers({
 		'Content-Type': 'application/json',
-		'x-csrf-token': window['Liferay'].authToken
-	})
-}
+		'x-csrf-token': window['Liferay'].authToken,
+	});
+};
 
 const baseFetch = async (url, {body, method = 'GET'} = {}) => {
-
-	let apiPath = process.env.REACT_APP_LIFERAY_API;
+	const apiPath = process.env.REACT_APP_LIFERAY_API;
 
 	const response = await fetch(apiPath + '/' + url, {
 		...(body && {body: JSON.stringify(body)}),
